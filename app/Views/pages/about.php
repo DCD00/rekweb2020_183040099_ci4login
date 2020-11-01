1b34a83533e44533153ed9e02f88e2c87fd05119
Drajat Cahya Diningrat <71449473+DCD00@users.noreply.github.com>
2020-11-01 20:39:33 +0700

    Materi Login, Logout, Register, User Activation

---
+++ app/Views/pages/about.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->Section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repudiandae nisi quos nostrum repellendus adipisci beatae ipsa aliquam modi totam illum blanditiis nobis vero veritatis, eveniet pariatur at ab eum.</p>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
